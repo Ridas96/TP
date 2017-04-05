43d9e1169f529a591871d5e45861edbd605a9152
Ridas96 <chachilisimo@gmail.com>
2017-04-05 18:30:32 +0200

    Add files via upload

---
+++ Estrellas.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "Estrellas.h"
+#include "Estrella.h"
+#include "Colision.h"
+#include "Personaje.h"
+struct EstrellasRep
+{
+    Estrella * estrella;
+    int max, n;
+};
+Estrellas EstrellasCrea(Imagen i,int w,int h,int x,int y,int max)
+{
+    Estrellas p =malloc(sizeof(struct EstrellasRep));
+    p->estrella=malloc(max*sizeof(Estrellas));
+    p->n=0;
+    return p;
+};
+void EstrellasLibera(Estrellas p)
+{
+    free(p->estrella);
+    free(p);
+};
+int EstrellasLleno(Estrellas p)
+{
+    return (p->n==p->max);
+
+};
+void EstrellasInserta(Estrellas p,Imagen i,int w,int h,int x,int y)
+{
+    p->estrella[p->n]=EstrellaCrea(i,w,h,x,y);
+    p->n++;
+};
+void EstrellasDibuja(Estrellas p)
+{
+
+
+};
+int EstrellasColision(Estrellas p,int x, int y, int w, int h)//la coordenadas de la estrella(del personaje) desde el main la llamare como get x, get y, etc
+{
+    int i = 0;
+    while (i < p->n)
+    {
+        if (Colision(x,y,w,h,EstrellaGetx(p->estrella[i]),EstrellaGety(p->estrella[i] ),EstrellaGetw(p->estrella[i]),EstrellaGeth(p->estrella[i])))//compruebo si colisionan con estrella iesima
+        {
+            p->estrella[i] = p->n[p->n-1];
+            p->n--;
+        }
+        else i++;
+    }
+//bucle que recorra cada estrella para saber si hay colision
+//bucle colision del estilo conjunto suprime maximo
+
+}
---
+++ Estrellas.h
@@ -0,0 +1,18 @@
+#ifndef __Estrellas_H
+#define __Estrellas_H
+#include "Pantalla.h"
+
+typedef struct EstrellasRep *Estrellas;
+
+Estrellas EstrellasCrea(Imagen i,int w,int h,int x,int y,int max);
+void EstrellasLibera(Estrellas p);
+void EstrellasDibuja(Estrellas p);
+int EstrellasColision(Estrellas p,int w,int h,int x,int y);
+void EstrellasInserta(Estrellas p,Imagen i,int w,int h,int x,int y);
+int EstrellasLleno(Estrellas p);
+int EstrellasGetx(Estrellas p); //devuelve la x del personaje
+int EstrellasGety(Estrellas p); //devuelve la y del personaje
+int EstrellasGetw(Estrellas p);
+int EstrellasGeth(Estrellas p);
+
+#endif
