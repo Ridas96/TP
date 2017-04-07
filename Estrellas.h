2971402f96d1f32319c85992964aab6de28d8600
Ridas96 <chachilisimo@gmail.com>
2017-04-07 10:20:49 +0200

    Update Estrellas.h

--- Estrellas.h
+++ Estrellas.h
@@ -1,15 +1,14 @@
 #ifndef __Estrellas_H
 #define __Estrellas_H
 #include "Pantalla.h"
-
+#include "Estrella.h"
 typedef struct EstrellasRep *Estrellas;
-typedef int Elemento;
 
 Estrellas EstrellasCrea(int max);
 void EstrellasLibera(Estrellas p);
 void EstrellasDibuja(Estrellas p);
 int EstrellasColision(Estrellas p,int w,int h,int x,int y);
-void EstrellasInserta(Estrellas p,Imagen i,int w,int h,int x,int y);
+void EstrellasInserta(Estrellas p,Estrella estrella);
 int EstrellasLleno(Estrellas p);
 int EstrellasGetx(Estrellas p); //devuelve la x del personaje
 int EstrellasGety(Estrellas p); //devuelve la y del personaje
